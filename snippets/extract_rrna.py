bd61e69a568321beb8264782221c32117be53aca
abulovic <bulovic.ana@gmail.com>
2013-06-29 13:17:14 +0200

    Extract RNA a bit neater.

--- snippets/extract_rrna.py
+++ snippets/extract_rrna.py
@@ -10,28 +10,29 @@ from ncbi.db.data_access import DataAccess
 from ncbi.taxonomy.tree import TaxTree
 from data.containers.read import ReadContainer
 from data.containers.record import RecordContainer
+from data.containers.cdsaln import CdsAlnContainer
 import filters.host as host_filter
 from utils import timeit
+from utils.location import Location
 
-class ExtractRnasParser(DefaultBinnerArgParser):
+class TestRunArgParser(DefaultBinnerArgParser):
     def __init__(self):
-        super(ExtractRnasParser, self).__init__('''Loads input alignment file,\
-        ... filteres host reads and produces file with all records found \
-        ...among rrnas.'''
-        )
-        self.add_argument('input', help='Input alignment file', 
-                           type=str)
-        self.add_argument('output', help='Output file with list of found rna accessions.',
-                           type=str)
-        self.add_argument('-dh', '--determine_host', help='determine host method',
-            choices=['best_score_host', '50perc_host', 'all_host'],
-            default = '50perc_host')
+        super(TestRunArgParser, self).__init__('''\
+                Loads input alignment file, fills read container, \
+                filters host reads and fills record container \
+                with read from specified database table.''')
+        self.add_argument('input',
+                help='input alignment file',
+                type=str)
+        self.add_argument('output',
+                help='output file with rna information',
+                type=str)
+
 
 
 def log_start (log, args):
-    log.info('EXTRACT RRNA RUN')
+    log.info('BINNER RUN')
     log.info("Input: %s" % args.input)
-    log.info("Output: %s" %  args.output)
 
 
 
@@ -46,40 +47,29 @@ def main():
 
     #----------------------------------#
     #------ INPUT ARGUMENTS -----------#
-    # 1. input alignment file
-    # 2. output rrna accession file
-    argparser = ExtractRnasParser()
+    argparser = TestRunArgParser()
     args  = argparser.parse_args()
-
-    #----------------------------------#
-    #------- DATA LOGGING INIT --------#
-    log = logging.getLogger(__name__)
-    logging.config.fileConfig(args.log_configuration,
-                              disable_existing_loggers=False)
-    log_start(log, args)
-
 
     #----------------------------------#
     #------- STATIC DATA SOURCE -------#
     # CDS - GI2TAXID -- NAMES -- NODES #
     dataAccess = DataAccess(args)
-    raw_input('Data access created')
+    #raw_input('Data access created')
     #----------------------------------#
 
     #-------- TAXONOMY TREE -----------#
     tax_tree = TaxTree()
     # tax_tree.load_taxonomy_data(dataAccess)
-    raw_input('Tax tree loaded.')
+    #raw_input('Tax tree loaded.')
 
     #----------------------------------#
     #------- ALIGNMENT DATA SOURCE ----#
     read_container = ReadContainer()
-    timeit(read_container.load_alignment_data, input_aln_file)
-    raw_input('Alignment file loaded')
-    print 'Number of PREFILTERED reads: ', len(read_container.fetch_all_reads(format=list))
-    timeit(read_container.set_taxids, dataAccess)
-    raw_input('Data access set')
+    read_container.load_alignment_data(args.input)
+    #---SET TAXIDS FOR ALL ALIGNMENTS--#
+    read_container.set_taxids(dataAccess)
 
+    #------- FILTER HOST READS -------#
     new_reads = host_filter.filter_potential_host_reads(
         read_container.fetch_all_reads(format=list),
         tax_tree.tax2relevantTax,
@@ -91,22 +81,46 @@ def main():
         #unassigned_taxid=
         -1,
         host_filter.is_best_score_host)
-
     read_container.set_new_reads(new_reads)
-    raw_input('Reads filtered.')
-    print 'Number of FILTERED reads: ', len(read_container.fetch_all_reads(format=list))
-    dataAccess.clear_cache()
+    dataAccess.clear_cache()    # deletes gi2taxid cache
 
+    #----------------------------------#
+    #------- LOAD ALL RECORDS   -------#
     record_container = RecordContainer()
     record_container.set_db_access(dataAccess)
     record_container.populate(read_container.fetch_all_reads_versions(), table='rrna')
-    raw_input('Records fetched')
+    #----------------------------------#
+    #-- MAP ALIGNMENTS TO GENES   -----#
+    read_container.populate_cdss(record_container)
+    #----------------------------------#
+    #- RECORD ALL ALIGNEMENTS TO GENE -#
+    cds_aln_container = CdsAlnContainer()
+    cds_aln_container.populate(read_container.fetch_all_reads(format=list))
 
-    output_rrna_accession_file = open(output_rrna_accession_file, 'w')
-    for record in record_container.fetch_all_records():
-        if record:
-            output_rrna_accession_file.write('%s\n' % (record.version))
-    output_rrna_accession_file.close()
+    output_file = open(args.output, 'w')
+
+    for cds_aln in cds_aln_container.fetch_all_cds_alns():
+        output = cds_aln.cds.version + ','
+        if cds_aln.cds.gene is not None:
+            output += 'gene:%s,' % cds_aln.cds.gene
+        location = Location.from_location_str(cds_aln.cds.location)
+        if location.start is not None:
+            aln_loc = '(%d,%d)' % (location.start, location.end)
+        else:
+            aln_loc = ''
+            for subloc in location.sublocations:
+                aln_loc += '(%d,%d),' % (subloc.start, subloc.end)
+        output += '%s:' % aln_loc
+        for aln_subloc in cds_aln.aligned_regions.values():
+            if aln_subloc.location.start is None or aln_subloc.location.end is None:
+                aln_str = ''
+                for subloc in aln_subloc.location.sublocations:
+                    aln_str += '(%d,%d),' % (subloc.start, subloc.end)
+            else: aln_str = '(%d,%d)' % (aln_subloc.location.start, aln_subloc.location.end)
+            output += "%s,%.1f,%s;" % (aln_subloc.read_id, aln_subloc.score, aln_str)
+        output_file.write('%s\n' % output)
+
+    output_file.close()
     log.info('BINNER EXIT')
 
 
