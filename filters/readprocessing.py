adf197008a434af93aa14d86e00534a58da26fdb
abulovic <bulovic.ana@gmail.com>
2013-07-02 17:26:44 +0200

    Fixed some bugs in annotation.

--- filters/readprocessing.py
+++ filters/readprocessing.py
@@ -1,4 +1,4 @@
-
+from utils import enum
 #------------------------------------------------------------------------#
 #------------ READ ANNOTATIONS REGRADING [ORGANISM/ALIGNMENT] -----------#
 alignment_count_status = enum (                             #0b0000000xx
@@ -20,13 +20,13 @@ organism_type_status  = enum  (                             #0b0000xx0000
                                 MASK                        =0b0000011000
                               )
 coding_region_aln_count_status = enum (                     #0b00xx000000
-                                NO_CODING_ALIGNMENTS        =0b0000000000
+                                NO_CODING_ALIGNMENTS        =0b0000000000,
                                 ONE_CODING_ALIGNMENT        =0b0001000000,
                                 MULTIPLE_CODING_ALIGNMENTS  =0b0010000000,
                                 MASK                        =0b0011000000
                               )
 coding_region_alignment_status = enum (                     #0bxx00000000
-                                ONE_TARGET_ORGANIM          =0b0000000000,
+                                ONE_TARGET_ORGANISM         =0b0000000000,
                                 MULTIPLE_TARGET_ORGANISMS   =0b0100000000,
                                 NONTARGET_ORGANISMS         =0b1000000000,
                                 MIXED_ORGANISMS             =0b1100000000,
@@ -80,12 +80,12 @@ def mark_single_alignment_read(read, read2cds_repository, tax_tree, target_organ
         cds_aln_count = coding_region_aln_count_status.ONE_CODING_ALIGNMENT
     # coding region organism type
     if mapped_to_target:
-        cds_aln_type = coding_region_alignment_status.ONE_TARGET_ORGANIMS
+        cds_aln_type = coding_region_alignment_status.ONE_TARGET_ORGANISM
     else:
         cds_aln_type = coding_region_alignment_status.NONTARGET_ORGANISMS
 
     status = aln_count | org_count | org_type | cds_aln_count | cds_aln_type
-    return status
+    read.set_status(status)
 
 def mark_multiple_alignment_read(read, read2cds_repository, tax_tree, target_organisms):
     alignments = read.get_alignments(format=list)
@@ -108,6 +108,7 @@ def mark_multiple_alignment_read(read, read2cds_repository, tax_tree, target_org
     else:
         org_type = organism_type_status.MIXED
     # coding region alignment type
+    cds_aln_type = 0
     if not read2cds_repository.has_key(read.id):
         cds_aln_count = coding_region_aln_count_status.NO_CODING_ALIGNMENTS
     else:
@@ -116,23 +117,23 @@ def mark_multiple_alignment_read(read, read2cds_repository, tax_tree, target_org
             cds_aln_count = coding_region_aln_count_status.ONE_CODING_ALIGNMENT
         else:
             cds_aln_count = coding_region_aln_count_status.MULTIPLE_CODING_ALIGNMENTS
-    # coding region organism type
-    coding_alns_tax_ids = set()
-    for cds in cdss:
-        coding_alns_tax_ids.append(cds.taxon)
-    children_count = get_child_count(coding_alns_tax_ids, target_organisms, tax_tree)
-    if children_count == len(coding_alns_tax_ids):
-        if children_count == 1:
-            cds_aln_type = coding_region_alignment_status.ONE_TARGET_ORGANIMS
+        # coding region organism type
+        coding_alns_tax_ids = set()
+        for cds in cdss:
+            coding_alns_tax_ids.add(cds.cds.taxon)
+        children_count = get_child_count(coding_alns_tax_ids, target_organisms, tax_tree)
+        if children_count == len(coding_alns_tax_ids):
+            if children_count == 1:
+                cds_aln_type = coding_region_alignment_status.ONE_TARGET_ORGANISM
+            else:
+                cds_aln_type = coding_region_alignment_status.MULTIPLE_TARGET_ORGANISMS
+        elif children_count == 0:
+            cds_aln_type = coding_region_alignment_status.NONTARGET_ORGANISMS
         else:
-            cds_aln_type = coding_region_alignment_status.MULTIPLE_TARGET_ORGANISMS
-    elif children_count == 0:
-        cds_aln_type = coding_region_alignment_status.NONTARGET_ORGANISMS
-    else:
-        cds_aln_type = coding_region_alignment_status.MIXED_ORGANISMS
+            cds_aln_type = coding_region_alignment_status.MIXED_ORGANISMS
 
     status = aln_count | org_count | org_type | cds_aln_count | cds_aln_type
-    return status
+    read.set_status(status)
 
 
 
