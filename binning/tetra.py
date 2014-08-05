728e757d7135f9b1116e56e5983f091686f24176
abulovic <bulovic.ana@gmail.com>
2014-08-05 18:19:13 +0200

    binning.tetra: A whole lot of clustering functionality.
    Unfortunatelly, none of it seems to work very well.

--- binning/tetra.py
+++ binning/tetra.py
@@ -1,10 +1,17 @@
 import math
+import time
 
 from itertools import product
 from collections import defaultdict
 from contextlib import nested
 
 import numpy as np
+from sklearn import cluster, metrics
+from sklearn.decomposition import PCA, FactorAnalysis
+from sklearn.covariance import ShrunkCovariance, LedoitWolf
+from sklearn.cross_validation import cross_val_score
+from sklearn.grid_search import GridSearchCV
+from sklearn.preprocessing import StandardScaler
 from Bio import SeqIO
 
 from ncbi.taxonomy.tree import TaxTree
@@ -14,6 +21,70 @@ VIRTUAL_ZERO = 1e-10
 
 TETRAS = sorted(map(lambda l: ''.join(l), product('ATGC', repeat=4)))
 TRIPLETS = sorted(map(lambda l: ''.join(l), product('ATGC', repeat=3)))
+
+def compute_scores(X, n_components):
+    pca = PCA()
+    fa = FactorAnalysis()
+
+    pca_scores, fa_scores = [], []
+    for n in n_components:
+    	start = time.time()
+        pca.n_components = n
+        fa.n_components = n
+        pca_scores.append(np.mean(cross_val_score(pca, X)))
+        fa_scores.append(np.mean(cross_val_score(fa, X)))
+        end = time.time()
+        print 'PCA scores (%3d)' % n, pca_scores
+        print 'FA  scores (%3d)' % n, fa_scores
+        print 'TIME:           ', end-start
+
+    return pca_scores, fa_scores
+
+def perform_pca(data):
+	X = np.c_[data]
+	n_samples, n_features = X.shape
+
+	n_components = np.arange(0, n_features, 16)
+	pca_scores, fa_scores = compute_scores(X, n_components)
+	n_components_pca = n_components[np.argmax(pca_scores)]
+	n_components_fa = n_components[np.argmax(fa_scores)]
+
+	pca = PCA(n_components='mle')
+	pca.fit(X)
+	n_components_pca_mle = pca.n_components_
+
+	print("best n_components by PCA CV = %d" % n_components_pca)
+	print("best n_components by FactorAnalysis CV = %d" % n_components_fa)
+	print("best n_components by PCA MLE = %d" % n_components_pca_mle)
+
+def perform_spectral_clustering(data):
+	X = np.c_[data]
+	spectral = cluster.SpectralClustering(eigen_solver='arpack',
+										  affinity='nearest_neighbors')
+	y = spectral.fit_predict(X)
+	return y
+
+def perform_mean_shift(data):
+	X = np.c_[data]
+	(n_samples, n_features) = X.shape
+	bandwidth = cluster.estimate_bandwidth(X, n_samples=n_samples)
+	ms = cluster.MeanShift(bandwidth=bandwidth, bin_seeding=True, cluster_all=False)
+	ms.fit(X)
+	import pdb
+	pdb.set_trace()
+
+def perform_DBSCAN(data):
+	X = np.c_[data]
+	X = StandardScaler().fit_transform(X)
+	db = cluster.DBSCAN(eps=0.3, min_samples=100).fit(X)
+	core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+	core_samples_mask[db.core_sample_indices_] = True
+	labels = db.labels_
+
+	n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+	print('Estimated number of clusters: %d' % n_clusters_)
+
 
 def get_tetranucl_z_scores(seq):
 	freqs_quad = defaultdict(int)
@@ -95,3 +166,11 @@ def create_database(fasta_db, output_file, data_access):
 			fout.write('%s\n' % ' '.join(map(lambda f: '%.4f' % f, freqs)))
 			i += 1
 			print 'Processing %5d. sequence...' % i
+
+def get_zscores_from_fasta(fasta):
+	data = []
+	with open(fasta, 'r') as fin:
+		records = SeqIO.parse(fin, 'fasta')
+		for rec in records:
+			data.append(get_tetranucl_z_scores(str(rec.seq)))
+	return data
